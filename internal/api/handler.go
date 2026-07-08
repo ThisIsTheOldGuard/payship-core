9577a31c0dc2cfc3e4d3caf3c76fe6dac8947df4
ThisIsTheOldGuard <m-peresada001@yandex.ru>
2026-07-08 17:20:47 +0300

    Added Prometheus metrics: API request counter and request processing time histogram; added panic handling

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -46,7 +46,6 @@ type ErrorResponse struct {
 //	$ curl http://localhost:8080/
 //	PayShip API is running
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	_, _ = w.Write([]byte("PayShip API is running"))
